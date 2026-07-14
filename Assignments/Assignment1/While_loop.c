86f4183c78bcbd25772cb651311eb74e0d5a1202
shweta porje <149692616+shwetaporje0515@users.noreply.github.com>
2026-07-14 14:48:32 +0530

    Add files via upload

---
+++ Assignments/Assignment1/While_loop.c
@@ -0,0 +1,45 @@
+//3. Program to print 5 to 1 numbers on screen. 
+
+#include<stdio.h>
+
+int Display()
+{
+	int i = 0;
+	//int i = 5;
+	while(i>=5)
+	{
+		printf("%d",i);
+		i++;
+	}
+}
+
+int main()
+{
+	Display();
+
+	return 0;
+}
+
+
+
+//3. Program to print 5 to 1 numbers on screen. 
+
+//#include<stdio.h>
+
+//int Display()
+//{
+// 	int i = 0;
+//	int i = 5;
+//	while(i>=1)
+//	{
+//		printf("%d\n",i);
+//		i--;
+//	}
+//}
+
+//int main()
+//{
+//	Display();
+//
+//	return 0;
+//}
