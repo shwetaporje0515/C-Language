5103f708c61493cf6da61f21c555699ee41afa86
shweta porje <149692616+shwetaporje0515@users.noreply.github.com>
2026-07-23 01:24:19 +0530

    Fix range validation and clean up code
    Updated the range validation logic to check if the start is greater than or equal to the end. Adjusted the comments and cleaned up the code formatting.

--- Assignments/Assignment11/2_DisplayRangeEven.c
+++ Assignments/Assignment11/2_DisplayRangeEven.c
@@ -1,4 +1,4 @@
-/2. Write a program which accept range from user and display all even numbers in 
+/*2. Write a program which accept range from user and display all even numbers in 
 between that range. 
 Input :  23 35  
 Output :  24 26 28 30 32 34   
@@ -10,18 +10,13 @@ Output :  10
 Input : -10 2 
 Output :  -10 -8 -6 -4 -2 0 2 
 Input : 90 18 
-Output :  Invalid range**/
+Output :  Invalid range*/
 
 #include<stdio.h>
 
 void RangeDisplayEven(int iStart, int iEnd)
 {   
-    if(iStart <= 0)
-    {
-        iStart = -iStart;
-    }
-    
-    if(iStart > iEnd)
+    if(iStart >= iEnd)
     {
         printf("Invalid range");
     }
