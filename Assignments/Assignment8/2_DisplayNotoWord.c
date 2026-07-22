693f58ba96fb05db846fc42b9629688f216776fe
shweta porje <149692616+shwetaporje0515@users.noreply.github.com>
2026-07-23 01:04:21 +0530

    Update 2_DisplayNotoWord.c

--- Assignments/Assignment8/2_DisplayNotoWord.c
+++ Assignments/Assignment8/2_DisplayNotoWord.c
@@ -13,30 +13,42 @@ void Display(int iNo)
 {
     switch(iNo)
     {
+        case 0:
+            printf("zero");
+            break;
+        case -1 :
         case 1:
             printf("One");
             break;
+        case -2:
         case 2:
             printf("Two");
             break;
+        case -3:
         case 3:
             printf("Three");
             break;
+        case -4:
         case 4:
             printf("Four");
             break;
+        case -5:
         case 5 :
             printf("Five");
             break;
+        case -6:
         case 6 :
             printf("Six");
             break;
+        case -7:
         case 7 :
             printf("Seven");
             break;
+        case -8:
         case 8 :
             printf("Eight");
             break;
+        case -9:
         case 9 : 
             printf("Nine");
             break;
